7dcbad47044ea59b99224d3fe154db405f004e64
Alex Barth <AlexBarthGlobal@gmail.com>
2021-10-10 15:02:59 -0400

    Doing some CSS on slider to create a track

--- src/components/BPMSlider.js
+++ src/components/BPMSlider.js
@@ -25,19 +25,22 @@ export default class BPMSlider extends React.Component {
         return (
             <div>
                 <div className='BPMText'>{`${this.state.localBPM}`}</div>
-                <ReactSlider
-                    className="horizontalSlider"
-                    thumbClassName="exampleThumb"
-                    trackClassName="exampleTrack"
-                    renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
-                    min={80}
-                    max={200}
-                    value={this.props.BPM}
-                    // step={1}
-                    onChange={(value, index) => this.setState({localBPM: value})}
-                    onAfterChange={(value, index) => this.props.changeBPM(value)}
-                    // withTracks={true}
-                />
+                <div>
+                <div className='exampleTrack'></div>
+                    <ReactSlider
+                        className="horizontalSlider"
+                        thumbClassName="exampleThumb"
+                        // trackClassName="exampleTrack"
+                        renderThumb={(props, state) => <div {...props}></div>}
+                        min={80}
+                        max={200}
+                        value={this.props.BPM}
+                        // step={1}
+                        onChange={(value) => this.setState({localBPM: value})}
+                        onAfterChange={(value) => this.props.changeBPM(value)}
+                        // withTracks={true}
+                    />
+                </div>
             </div>
         )
     }

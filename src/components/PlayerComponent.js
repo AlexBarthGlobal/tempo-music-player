e9339ce9fe44f94ca5d2a5855be88f3ef6ad168c
Alex Barth <AlexBarthGlobal@gmail.com>
2021-11-03 03:55:41 -0400

    Moved the metronome on collectionSongs down a bit because it was too close to the title

--- src/components/PlayerComponent.js
+++ src/components/PlayerComponent.js
@@ -6,25 +6,25 @@ const PlayerComponent = (props) => {
 
     return (
         <div id='mainPlayerContainer'>
-        <div id='mainPlayer'>
-            <div id='innerMainPlayerContainer'>
-            <div id='innerMainPlayer'>
-                <div id='mainPlayerImageContainer'>
-                    <img id='mainPlayerImg' src='https://images.genius.com/ecb6201ab5498a21dac43d928de2d127.499x499x1.png'/>
-                </div>
-                <div id='mainPlayerSongInfo'>
-                    <div>Proud</div>
-                    <div>Money Man</div>
-                    <div>6 Hours 2</div>
-                    <div>Playing from: Beach Chill</div>
-                </div>
-                <div className='mainPlayerFlexCenterVertical'>
-                    <MainPlayerSlider play={props.play} pause={props.pause} playing={props.playing} nextTrack={props.nextTrack} currTime={props.currTime} duration={props.duration} seekTime={props.seekTime} />
-                    <AllInclusiveIcon onClick={props.toggleLoop} className={`loopMarginTop ${props.isLooping ? 'loopOn' : null}`} />
+            <div id='mainPlayer'>
+                <div id='innerMainPlayerContainer'>
+                    <div id='innerMainPlayer'>
+                        <div id='mainPlayerImageContainer'>
+                            <img id='mainPlayerImg' src='https://images.genius.com/ecb6201ab5498a21dac43d928de2d127.499x499x1.png'/>
+                        </div>
+                        <div id='mainPlayerSongInfo'>
+                            <div>Proud</div>
+                            <div>Money Man</div>
+                            <div>6 Hours 2</div>
+                            <div>Playing from: Beach Chill</div>
+                        </div>
+                        <div className='mainPlayerFlexCenterVertical'>
+                            <MainPlayerSlider play={props.play} pause={props.pause} playing={props.playing} nextTrack={props.nextTrack} currTime={props.currTime} duration={props.duration} seekTime={props.seekTime} />
+                            <AllInclusiveIcon onClick={props.toggleLoop} className={`loopMarginTop ${props.isLooping ? 'loopOn' : null}`} />
+                        </div>
+                    </div>
                 </div>
             </div>
-            </div>
-        </div>
         </div>
     )
 }

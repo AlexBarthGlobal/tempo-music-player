3a8037e8352d80d0430affe17a748aeb5e7803c6
Alex Barth <AlexBarthGlobal@gmail.com>
2021-11-04 18:33:00 -0400

    I'm using a table for collectionSongs

--- src/components/CollectionSingleSong.js
+++ src/components/CollectionSingleSong.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import { isBrowser, isMobile } from 'react-device-detect';
+import secondsToTimestamp from './secondsToTimestamp'
 
 const CollectionSingleSong = (props) => {
     const {songId, songName, artistName, albumName, BPM, duration, artURL, editMode, removeSongFromCollection, listenedBool} = props
-    //convert duration into minutes & seconds
-
-
 
         return (
-            <li>
-                {songName} {artistName} {albumName} {BPM} {duration} {editMode ? null : listenedBool ? 'Listened' : null} {editMode ? <button onClick={() => removeSongFromCollection(songId)}>Remove</button> : null}
-            </li>
+            <tr>
+                <td className='singleSongRemoveBox'>{editMode ? <div onClick={() => removeSongFromCollection(songId)}>X</div> : null}</td>
+                <td className='singleSongInfo'>
+                    <img className='collectionSongImg' src={artURL} />
+                    <div className='singleSongVertical'>
+                        <div>{songName}</div>
+                        <div>{artistName}</div>
+                    </div>
+                </td>
+                {isBrowser ? <td>{albumName}</td> : null}
+                <td>{BPM}</td>
+                <td>{secondsToTimestamp(duration)}</td>
+            </tr>
         )
 };
 

311545d296a71f0cf2077cbfd0eeb657c6091c65
Alex Barth <AlexBarthGlobal@gmail.com>
2021-11-04 23:33:35 -0400

    browseSongs basic styling done

--- src/components/BrowseSongsSingleSong.js
+++ src/components/BrowseSongsSingleSong.js
@@ -3,10 +3,10 @@ import { isBrowser, isMobile } from 'react-device-detect';
 import secondsToTimestamp from './secondsToTimestamp'
 import AddIcon from '@mui/icons-material/Add';
 import ClearIcon from '@mui/icons-material/Clear';
+import PlayCircleFilledWhiteOutlinedIcon from '@mui/icons-material/PlayCircleFilledWhiteOutlined';
+import StopIcon from '@mui/icons-material/Stop';
 
-{/* <button onClick={() => selectSong(songURL)}>{playingURL === songURL ? 'Stop' : 'Play'}</button>
-<button onClick={!inCollection ? () => addSongToCollection(songId) : () => removeSongFromCollection(songId)}>{!inCollection ? 'Add' : 'Remove'}</button> */}
-
+{/* <button onClick={() => selectSong(songURL)}>{playingURL === songURL ? 'Stop' : 'Play'}</button>*/}
 
 const BrowseSongsSingleSong = (props) => {
     const {songId, songName, artistName, albumName, BPM, duration, songURL, artURL, addSongToCollection, removeSongFromCollection, inCollection, playingStatus, playingURL, selectSong} = props
@@ -14,7 +14,10 @@ const BrowseSongsSingleSong = (props) => {
         <tr>
             <td className='removeSongCrossContainer'>{!inCollection ? <AddIcon className={isBrowser ? 'removeSongCross' : 'removeSongCrossMobile'} onClick={() => addSongToCollection(songId)} /> : <ClearIcon className={isBrowser ? 'removeSongCross' : 'removeSongCrossMobile'} onClick={() => removeSongFromCollection(songId)}/>}</td>
             <td className='singleSongInfo'>
-                <img className='collectionSongImg' src={artURL} />
+                <div className='previewControlsContainer'>
+                    <img className='collectionSongImg' src={artURL} />
+                    {playingURL === songURL ? <StopIcon onClick={() => selectSong(songURL)} className='previewControls' /> : <PlayCircleFilledWhiteOutlinedIcon onClick={() => selectSong(songURL)} className='previewControls' />}
+                </div>
                 <div className='singleSongVertical centerVertical touchPaddingBottomSong'>
                     <div>{songName}</div>
                     <div>{artistName}</div>

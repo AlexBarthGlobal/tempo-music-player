67e27e4048d9554b4fe59a3800ed06c65c42a261
Alex Barth <AlexBarthGlobal@gmail.com>
2021-11-09 17:04:39 -0500

    Adding more box shadow on login screen

--- server/db/database.js
+++ server/db/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const env = process.env.NODE_ENV || 'production';
+const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 
 const db = new Sequelize(config.database, config.username, config.password, config);
